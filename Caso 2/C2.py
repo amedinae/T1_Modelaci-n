bdb4fda662913921e9a36b20034a10e9e23f157f
Andres Medina <amedinae@unal.edu.co>
2020-11-04 21:36:43 -0500

    Optimización al rungeKutta del caso 3

---
+++ Caso 2/C2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  4 20:07:19 2020
+
+@author: Andres Medina
+"""
+
--- Caso 3/C3TemperaturaDependiente.py
+++ Caso 3/C3TemperaturaDependiente.py
@@ -24,26 +24,18 @@ def velocidad_reaccion(T):
             kr[line] = A*T**b*np.exp(-Ea/(Ru*T))
     return kf,kr    
 
-def funciones(t,x,i):
-    #x --> [x0,x1,x2,x3,x4,x5,x6,x7]
+def dXdt(x):
     [[kf1,kf2,kf3,kf4,kf5,kf6],[kr1,kr2,kr3,kr4,kr5,kr6]] = velocidad_reaccion(x[7])
-    if(i==0):
-        f = -kf1*x[0]*x[1] + kr1*x[2]*x[3] + kf2*x[2]*x[4] - kr2*x[0]*x[3] + kf3*x[3]*x[4]-kr3*x[0]*x[5] - kf5*x[0]*x[6] + kr5*x[4]*x[1]
-    elif(i==1):
-        f = -kf1*x[0]*x[1] + kr1*x[2]*x[3] + kf5*x[0]*x[6] - kr5*x[4]*x[1] + kf6*x[2]*x[6]- kr6*x[3]*x[1]
-    elif(i==2):
-        f = kf1*x[0]*x[1] - kr1*x[2]*x[3] - kf2*x[2]*x[4] + kr2*x[0]*x[3] - kf4*x[2]*x[5] + kr4*(x[3]**2) - kf6*x[2]*x[6] + kr6*x[3]*x[1]
-    elif(i==3):
-        f = kf1*x[0]*x[1] - kr1*x[2]*x[3] +  kf2*x[2]*x[4] - kr2*x[0]*x[3] - kf3*x[3]*x[4] + kr3*x[0]*x[5] + 2*kf4*x[2]*x[5] - 2*kr4*(x[3]**2) + kf6*x[2]*x[6] - kr6*x[3]*x[1] 
-    elif(i==4):
-        f = - kf2*x[2]*x[4] + kr2*x[0]*x[3] - kf3*x[3]*x[4] + kr3*x[0]*x[5] + kf5*x[0]*x[6] - kr5*x[4]*x[1]
-    elif(i==5):
-        f = kf3*x[3]*x[4]-kr3*x[0]*x[5] - kf4*x[2]*x[5] + kr4*(x[3]**2)
-    elif(i==6):
-        f = - kf5*x[0]*x[6] + kr5*x[4]*x[1] - kf6*x[2]*x[6] + kr6*x[3]*x[1]
-    elif(i==7):
-        f=-(1/x[4])*(- kf2*x[2]*x[4] + kr2*x[0]*x[3] - kf3*x[3]*x[4] + kr3*x[0]*x[5] + kf5*x[0]*x[6] - kr5*x[4]*x[1])*x[7]
-    return f
+    return np.array([
+                     -kf1*x[0]*x[1] + kr1*x[2]*x[3] + kf2*x[2]*x[4] - kr2*x[0]*x[3] + kf3*x[3]*x[4]-kr3*x[0]*x[5] - kf5*x[0]*x[6] + kr5*x[4]*x[1],
+                     -kf1*x[0]*x[1] + kr1*x[2]*x[3] + kf5*x[0]*x[6] - kr5*x[4]*x[1] + kf6*x[2]*x[6]- kr6*x[3]*x[1],
+                     kf1*x[0]*x[1] - kr1*x[2]*x[3] - kf2*x[2]*x[4] + kr2*x[0]*x[3] - kf4*x[2]*x[5] + kr4*(x[3]**2) - kf6*x[2]*x[6] + kr6*x[3]*x[1],
+                     kf1*x[0]*x[1] - kr1*x[2]*x[3] +  kf2*x[2]*x[4] - kr2*x[0]*x[3] - kf3*x[3]*x[4] + kr3*x[0]*x[5] + 2*kf4*x[2]*x[5] - 2*kr4*(x[3]**2) + kf6*x[2]*x[6] - kr6*x[3]*x[1],
+                     - kf2*x[2]*x[4] + kr2*x[0]*x[3] - kf3*x[3]*x[4] + kr3*x[0]*x[5] + kf5*x[0]*x[6] - kr5*x[4]*x[1],
+                     kf3*x[3]*x[4]-kr3*x[0]*x[5] - kf4*x[2]*x[5] + kr4*(x[3]**2),
+                     - kf5*x[0]*x[6] + kr5*x[4]*x[1] - kf6*x[2]*x[6] + kr6*x[3]*x[1],
+                     -(1/x[4])*(- kf2*x[2]*x[4] + kr2*x[0]*x[3] - kf3*x[3]*x[4] + kr3*x[0]*x[5] + kf5*x[0]*x[6] - kr5*x[4]*x[1])*x[7]
+                     ])
  
 def euler(fi_in,n,dt,t,T):
     #fi_in = [x0,x1,x2,x3,x4] del tiempo conocido
@@ -55,51 +47,20 @@ def euler(fi_in,n,dt,t,T):
         fi_out[i] = fi_in[i] + funciones(t,fi_in,T,i)*dt
     return fi_out
 
-def kutta(x,n,dt,t):
+def kutta4(x,dt):
+    k1 = dXdt(x)
+    k2 = dXdt(x+1/2*k1*dt)
+    k3 = dXdt(x+1/2*k2*dt)
+    k4 = dXdt(x+k3*dt)
+    f_out = x + dt/6*(k1+2*k2+2*k3+k4)
+    return f_out
     
-    ## Paso 1
-    k1 = np.zeros(n)
-    for i in range(n):
-        k1[i] = funciones(t,x,i)
-    
-    ## Paso 2
-    k2 = np.zeros(n)
-    xk2 = np.zeros(n)
-    for i in range(n):
-        xk2[i] = x[i] + (k1[i]/2)*dt
-    for i in range(n):
-        k2[i] = funciones(t+dt/2,xk2,i)
-    
-    ## Paso 3
-    k3 = np.zeros(n)
-    xk3 = np.zeros(n)    
-    for i in range(n):
-        xk3[i] = x[i] + (k2[i]/2)*dt
-    for i in range(n):
-        k3[i] = funciones(t+dt/2,xk3,i)    
-    
-    ## Paso 4
-    k4 = np.zeros(n)
-    xk4 = np.zeros(n)    
-    for i in range(n):
-        xk4[i] = x[i] + k3[i]*dt
-    for i in range(n):
-        k4[i] = funciones(t+dt,xk4,i) 
-    
-    ## Paso final
-    xf = np.zeros(n)
-    for i in range(n):
-        xf[i]=x[i]+(dt/6)*(k1[i]+2*k2[i]+2*k3[i]+k4[i])
-                          
-    return xf
-    
-
 dt = 1e-9 #s
 tf = 30e-6#s
     
 it = int(tf/dt)
     
-t = np.zeros(it+1)
+t = np.linspace(0,tf,it+1)
 x = np.zeros((it+1,8))
     
 #filas son las iteraciones
@@ -113,13 +74,11 @@ P= 1.2 # atm
 
 x[0,7] = 1200 #K
 x[0,1] = ((xO2/(xH2+xO2)*P)/(R*(x[0,7])))*10**(-3) #mol/cm^3
-x[0,4] = ((xH2/(xH2+xO2)*P)/(R*(x[0,7])))*10**(-3) #mol/cm^3
-
-    
+x[0,4] = ((xH2/(xH2+xO2)*P)/(R*(x[0,7])))*10**(-3) #mol/cm^3  
 
 for i in range(1,it+1):
-    t[i] = t[i-1] + dt
-    x[i,:] = kutta(x[i-1,:],8,dt,t[i-1])
+    print(i)
+    x[i,:] = kutta4(x[i-1,:],dt)
     
 
 plt.plot(t,x[:,0],"-k",label="x_H")

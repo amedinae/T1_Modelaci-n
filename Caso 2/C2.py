4861ec0c40462f508a1aebab58b2bf32d30afbd9
Andres Medina <amedinae@unal.edu.co>
2020-11-05 18:25:25 -0500

    Sistema de ecuaciones para los compartimientos del caso 2

--- Caso 2/C2.py
+++ Caso 2/C2.py
@@ -5,3 +5,11 @@ Created on Wed Nov  4 20:07:19 2020
 @author: Andres Medina
 """
 
+
+
+def dXdt(x,):
+    return np.array([Q3*c3-Q4/V*x[0],
+                     Q5c5+Q4/V*x[0]+QA/V*x[3],
+                     QB/V*x{1}-2*QC/V*x[2],
+                     Q6*c6+QC/V*x[2]-QA/V/x[4]
+                     ])

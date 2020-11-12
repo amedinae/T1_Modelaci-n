71b085a90fdfbe7348420abc0bed0d6e3456d555
Andres Medina <amedinae@unal.edu.co>
2020-11-11 19:13:32 -0500

    Primera prueba de caso 1

--- Caso 1/C1.py
+++ Caso 1/C1.py
@@ -9,80 +9,70 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def d1dt(x):
-    Fd = 0.5*patm*Cd*Sb*vb**2 
-    Fm =
-    Wx =  
-    return np.array([Q3*c3-Q4/V*x[0],
-                     Q5*c5+Q4/V*x[0]+QA/V*x[3]-QB/V*x[1],
-                     QB/V*x[1]-2*QC/V*x[2],
-                     Q6*c6+QC/V*x[2]-QA/V*x[3]
+def d1dt(x,ma,mb,rhow,S,Sb,Cd,mu,theta,V,Vw0,rhoa0):
+    rho0 = 101325
+    M = 0.0289654
+    R = 8.31447
+    T0 = 288.15
+    L = 0.0065
+    g = 9.81
+    rhoatm = (rho0*M)/(R*T0)*(1-L*x[4]/T0)**((g*M)/(R*L)-1)
+    Fd = 0.5*rhoatm*Cd*Sb*x[1]**2 
+    Fm = mu*(ma+mb+x[0])*g*np.cos(theta)
+    Wx = (ma+mb+x[0])*g*np.sin(theta)
+    return np.array([-rhow*x[2]*S,
+                     -(2*Fd*Sb**3 + 2*Fm*Sb**3 + 2*Sb**3*Wx + S**3*x[2]**3*rhow + 2*Fd*Sb**2*x[1] + 2*Fm*Sb**2*x[1] + 2*Sb**2*x[1]*Wx - 2*x[3]*S*Sb**2*x[2] + 2*rhoatm*S*Sb**2*x[2] + 2*S*Sb**3*x[2]*rhow - 2*S**2*Sb*x[2]**2*rhow + S*Sb**2*x[2]**3*rhow - 2*Fd*S*Sb*x[2] - 2*Fm*S*Sb*x[2] - 2*S*Sb*x[2]*Wx - 2*S**2*Sb**2*x[2]**2*rhow - 2*S*Sb**2*x[1]*x[2]**2*rhow + 2*S**2*Sb*x[1]*x[2]**2*rhow)/(2*Sb*(S*x[2]*ma - Sb**2*mb - Sb**2*x[0] - Sb**2*ma + S*x[2]*mb + Sb*x[1]*ma + Sb*x[1]*mb + Sb*x[1]*x[0])),
+                     -(4*Fd*Sb**2*x[1]*ma + 4*Fd*Sb**2*x[1]*mb + 4*Fm*Sb**2*x[1]*ma + 4*Fm*Sb**2*x[1]*mb + 4*Fd*Sb**2*x[1]*x[0] + 4*Fm*Sb**2*x[1]*x[0] + 4*Sb**2*x[1]*Wx*ma + 4*Sb**2*x[1]*Wx*mb + 4*Sb**2*x[1]*Wx*x[0] - S**3*x[2]**3*ma*rhow - S**3*x[2]**3*mb*rhow + S**3*x[2]**3*x[0]*rhow - 2*Fd*S*Sb*x[2]*x[0] - 2*Fm*S*Sb*x[2]*x[0] - 2*S*Sb*x[2]*Wx*x[0] - 2*x[3]*S*Sb**2*x[2]*ma + 2*rhoatm*S*Sb**2*x[2]*ma - 2*x[3]*S*Sb**2*x[2]*mb + 2*rhoatm*S*Sb**2*x[2]*mb - 2*x[3]*S*Sb**2*x[2]*x[0] + 2*rhoatm*S*Sb**2*x[2]*x[0] + S*Sb**2*x[2]**3*ma*rhow + S*Sb**2*x[2]**3*mb*rhow - 2*S**2*Sb*x[2]**2*x[0]*rhow + S*Sb**2*x[2]**3*x[0]*rhow - 2*S*Sb**2*x[1]*x[2]**2*ma*rhow - 2*S*Sb**2*x[1]*x[2]**2*mb*rhow - 2*S*Sb**2*x[1]*x[2]**2*x[0]*rhow + 2*S*Sb**2*x[1]*x[2]*ma*rhow + 2*S*Sb**2*x[1]*x[2]*mb*rhow + 2*S*Sb**2*x[1]*x[2]*x[0]*rhow)/(2*S*x[0]*(S*x[2]*ma - Sb**2*mb - Sb**2*x[0] - Sb**2*ma + S*x[2]*mb + Sb*x[1]*ma + Sb*x[1]*mb + Sb*x[1]*x[0])),
+                     -( rhoa0*S)/( (rhoa0/rhow)*x[0] -rhoa0*V )*x[2]*x[4],
+                     x[1]*np.sin(theta)
                      ])
 
-def d2dt(x,rhow,S,ma,mb):   
-    return np.array([x[1],
-                     1/(2*x[0])*((A/a)**2-1)*(x[1])**2 - Q1/x[0]*(A/a)**2*x[1] + (A/a)**2*Q1**2/(2*x[0]) + A*g
-                     ])
+#def d2dt(x,rhow,S,ma,mb):   
+#    return np.array([x[1],
+#                     1/(2*x[0])*((A/a)**2-1)*(x[1])**2 - Q1/x[0]*(A/a)**2*x[1] + (A/a)**2*Q1**2/(2*x[0]) + A*g
+#                     ])
 
-def kutta4(x,sistema,Q2=0,Q3=0,Q5=0,Q6=0,V=0,c3=0,c5=0,c6=0,Q1=0,A=0,a=0,g=0):
-    k1 = sistema(x,Q1,Q2,Q3,Q5,Q6,V,c3,c5,c6,A,a,g)
-    k2 = sistema(x+1/2*k1*dt,Q1,Q2,Q3,Q5,Q6,V,c3,c5,c6,A,a,g)
-    k3 = sistema(x+1/2*k2*dt,Q1,Q2,Q3,Q5,Q6,V,c3,c5,c6,A,a,g)
-    k4 = sistema(x+k3*dt,Q1,Q2,Q3,Q5,Q6,V,c3,c5,c6,A,a,g)
+def kutta4(x,dt,ma,mb,rhow,S,Sb,Cd,mu,theta,V,Vw0,rhoa0):
+    k1 = d1dt(x,ma,mb,rhow,S,Sb,Cd,mu,theta,V,Vw0,rhoa0)
+    k2 = d1dt(x+1/2*k1*dt,ma,mb,rhow,S,Sb,Cd,mu,theta,V,Vw0,rhoa0)
+    k3 = d1dt(x+1/2*k2*dt,ma,mb,rhow,S,Sb,Cd,mu,theta,V,Vw0,rhoa0)
+    k4 = d1dt(x+k3*dt,ma,mb,rhow,S,Sb,Cd,mu,theta,V,Vw0,rhoa0)
     f_out = x + dt/6*(k1+2*k2+2*k3+k4)
     return f_out
 
-dt = 1e-3 #s
-tf = 40 #s
+dt = 1e-6 #s
+tf = 1
     
 it = int(tf/dt)
     
 t = np.linspace(0,tf,it+1)
-x1 = np.zeros((it+1,2))
+x1 = np.zeros((it+1,5))
 x2 = np.zeros((it+1,4))
-x1[0] = [30,0]
+x1[0] = [10,0,0,2,0]
 x2[0] = [1,1,1,1]
-A = 10;
-a = 1;
-g = -9.81;  
-Q1 = 5
-Q3 = 5
-Q5 = 5
-Q6 = 5
-c3 = 0.01
-c5 = 0.01
-c6 = 0.01
-V = 10
-alpha =0.4
-beta = 0.5
-QD = 10
 
-T0vacio = False
-tvaciado = it
+ma = 0.001
+mb = 0.1
+rhow = 1
+S = 0.1
+Sb = 0.001
+Cd = 0.1
+mu = 0.1
+theta = np.pi/2
+V = 2e-3 # m**3
+Vw0 = 1e-3 # m**3
+rhoa0 = 405300 #pascales
+
+h = np.zeros((it+1,1))
 
 for i in range(1,it+1):
-    if not T0vacio:
-        x1[i,:] = kutta4(x1[i-1,:],dQdt,Q1=Q1,A=A,a=a,g=g)
-    Q2 = Q1-x1[i-1,1]
-    x2[i,:] = kutta4(x2[i-1,:],dXdt,Q2,Q3,Q5,Q6,V,c3,c5,c6)
-    if x1[i,0]<1e-2 and not T0vacio:
-        tvaciado = i
-        T0vacio = True
-    
-plt.figure()
-plt.plot(t[0:tvaciado],x1[0:tvaciado:,0],"-b",label="Vol")    
-plt.plot(t[0:tvaciado],x1[0:tvaciado,1],"-g",label="dVol/dt")
-plt.legend(loc="upper right")
-plt.xlabel('tiempo(s)')
-plt.ylabel('Cambio de flujo volumetrico')
-plt.grid()
+    x1[i,:] = kutta4(x1[i-1,:],dt,ma,mb,rhow,S,Sb,Cd,mu,theta,V,Vw0,rhoa0)
+    if x1[i,0]<0:
+        break
 
 plt.figure()    
-plt.plot(t,x2[:,0],"-b",label="x0")    
-plt.plot(t,x2[:,1],"-g",label="x1")
-plt.plot(t,x2[:,2],"-y",label="x2")    
-plt.plot(t,x2[:,3],"-r",label="x3")
+plt.plot(t,x1[:,0],"-b",label="x0")    
 plt.legend(loc="upper right")
 plt.xlabel('tiempo(s)')
 plt.ylabel('Cantidad de soluto')
-plt.grid()
+plt.grid()    
